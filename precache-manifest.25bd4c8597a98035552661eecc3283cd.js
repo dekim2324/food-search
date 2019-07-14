c6cb818a8e754689617f853f945688466431c483
Dennis  Kim <denniskim@Denniss-MacBook-Pro.local>
2019-07-14 14:18:41 -0500

    Updates

---
+++ precache-manifest.25bd4c8597a98035552661eecc3283cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "508d564de5c462f689910936e174a829",
+    "url": "/food-search/index.html"
+  },
+  {
+    "revision": "d53cc3a7a21d491a4c12",
+    "url": "/food-search/static/css/main.0ee06368.chunk.css"
+  },
+  {
+    "revision": "d27ba6dadf7b6d6d23f2",
+    "url": "/food-search/static/js/2.aa9b191f.chunk.js"
+  },
+  {
+    "revision": "d53cc3a7a21d491a4c12",
+    "url": "/food-search/static/js/main.1e78c392.chunk.js"
+  },
+  {
+    "revision": "fd8cede8881afe40b70e",
+    "url": "/food-search/static/js/runtime~main.c5a2a9a6.js"
+  }
+]);
